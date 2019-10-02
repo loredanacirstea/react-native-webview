19da141431e58fb7df9435548f370843979d54e0
Bruno Barbieri <bruno.barbieri@consensys.net>
2019-10-01 22:54:26 -0400

    Unmount properly on android

--- android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
+++ android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
@@ -1219,7 +1219,9 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
     }
 
     protected void cleanupCallbacksAndDestroy() {
+      this.detachAllViewsFromParent();
       setWebViewClient(null);
+      this.removeAllViews();
       destroy();
     }
 
